f3e036a83dd0a50fc5d4733ca8e4de6eee1b3aca
Hasan YUCEL <hsnnycll@gmail.com>
2023-02-25 09:56:54 +0300

    add new file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,13 @@
-package org.example;public class Main {
+package org.example;
+import java.sql.*;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            Question5.answer();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
